239cd0bd8be1a0db9f7cef62a6244860f8e0dab0
Zero <Zero@hotmail.com>
2023-10-28 14:08:15 -0500

    finish2

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,19 +1,22 @@
-import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { LoginPage } from "./Pages/LoginPage";
 import { RegisterPage } from "./Pages/RegisterPage";
 import { TaskPage } from "./Pages/TaskPage";
 
 export const Rutas = () => {
-  const isAuthenticated = !!localStorage.getItem("token"); // Cambia esto según tu lógica de autenticación.
+  //const isAuthenticated = !!localStorage.getItem("token"); // Cambia esto según tu lógica de autenticación.
+  /*
+     <Route path="/Task" element={isAuthenticated ? <TaskPage /> : <Navigate to="/" />  }
+          />
 
+   */
   return (
     <BrowserRouter>
       <main>
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/registro" element={<RegisterPage />} />
-          <Route path="/Task" element={isAuthenticated ? <TaskPage /> : <Navigate to="/" />  }
-          />
+          <Route path="/Task" element={<TaskPage />} />
         </Routes>
       </main>
     </BrowserRouter>
